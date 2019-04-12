08f45b80f03f323971421196705ae13b6cb299e1
Manuel Sousa <manuelsousamvs@gmail.com>
2019-04-12 16:44:53 +0100

    Final delivery

--- notary-client/src/main/java/pt/ulisboa/tecnico/sec/notaryclient/NotaryAbstract.java
+++ notary-client/src/main/java/pt/ulisboa/tecnico/sec/notaryclient/NotaryAbstract.java
@@ -38,7 +38,7 @@ class NotaryAbstract {
     public NotaryAbstract(PrivateKey privateKey) {
         this.privateKey = privateKey;
         this.lastNotaryNonce = System.currentTimeMillis();
-        this.withCC = false;
+        this.withCC = true;
 
         try {
             String path = new File(System.getProperty("user.dir")).getParent();
--- notary/src/main/java/pt/ulisboa/tecnico/sec/notary/jaxrs/application/Bootstrap.java
+++ notary/src/main/java/pt/ulisboa/tecnico/sec/notary/jaxrs/application/Bootstrap.java
@@ -16,7 +16,7 @@ import java.security.PublicKey;
 
 public class Bootstrap implements ServletContextListener {
     private String serializeFileName = "notary.ser";
-    private boolean CITIZEN_CARD_ACTIVATED = false;
+    private boolean CITIZEN_CARD_ACTIVATED = true;
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
